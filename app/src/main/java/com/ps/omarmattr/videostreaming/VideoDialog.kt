78a9f94a942846645e0d9078f879c7924ae38f9c
omarmattr <omarabomattr@gmail.com>
2021-06-11 23:22:06 +0300

    update library to support offloading

--- app/src/main/java/com/ps/omarmattr/videostreaming/VideoDialog.kt
+++ app/src/main/java/com/ps/omarmattr/videostreaming/VideoDialog.kt
@@ -1,5 +1,6 @@
 package com.ps.omarmattr.videostreaming
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +8,11 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import cn.jzvd.JZVideoPlayer
-import cn.jzvd.JZVideoPlayerStandard
-import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.ps.omarmattr.videostreaming.databinding.VideoDialogBinding
 
 class VideoDialog :DialogFragment() {
@@ -16,6 +20,10 @@ class VideoDialog :DialogFragment() {
         VideoDialogBinding.inflate(layoutInflater)
     }
 
+    private var simpleExoplayer: SimpleExoPlayer? = null
+    private val dataSourceFactory: DataSource.Factory by lazy {
+        DefaultDataSourceFactory(requireContext(), "exoplayer-sample")
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -36,22 +44,29 @@ class VideoDialog :DialogFragment() {
         }
     }
     private fun uriVideo(uri: String, image: String) {
-        val video = mBinding.video
-        try {
-            video.setUp(
-                uri,
-                JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,
-                ""
-            )
-            Glide.with(this).load(image)
-                .into(video.thumbImageView)
-
-        } catch (e: Exception) {
-
-        }
+//        val video = mBinding.video
+//        try {
+//            video.setUp(
+//                uri,
+//                JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL,
+//                ""
+//            )
+//            Glide.with(this).load(image)
+//                .into(video.thumbImageView)
+//
+//        } catch (e: Exception) {
+//
+//        }
+        simpleExoplayer = SimpleExoPlayer.Builder(requireContext()).build()
+        preparePlayer(uri)
+        mBinding.video.player = simpleExoplayer
+        simpleExoplayer?.playWhenReady = true
     }
 
-
+    private fun preparePlayer(videoUrl: String) {
+        simpleExoplayer?.prepare(ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.parse(videoUrl)))
+    }
 
     override fun onPause() {
         super.onPause()
